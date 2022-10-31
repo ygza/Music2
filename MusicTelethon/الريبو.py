1b834ff9705d32fd15d8703cce046c5e034ae903
ygza <102256731+ygza@users.noreply.github.com>
2022-10-31 07:27:16 +0300

    Update الريبو.py

--- MusicTelethon/الريبو.py
+++ MusicTelethon/الريبو.py
@@ -33,7 +33,7 @@ async def restart(client, m: Message):
     await loli.edit("**✅ تم اعاده تشغيل موسيقى العبقري**")
     os.execl(sys.executable, sys.executable, *sys.argv)
     quit()
-@Client.on_message(filters.command(["العبقري"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["العبقري|طبطب"], prefixes=f"{HNDLR}"))
 async def help(client, m: Message):
     await m.delete()
     HELP = f"""
